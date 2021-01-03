1423a4fc2341f5364236ad910468a4b0edc90a53
Rohan Thorat <rthorat1@binghamton.edu>
2021-01-02 22:28:21 -0500

    adding reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ssn';
+  ssnSample = new FormGroup({
+    ssn: new FormControl('')
+  })
 }
