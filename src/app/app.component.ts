febca96b6054a686ddd14c98bd1a0050edd6527e
Rohan Thorat <rthorat1@binghamton.edu>
2021-01-03 22:54:44 -0500

    adding input formatting of ssn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +7,38 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ssn';
-  ssnSample = new FormGroup({
-    ssn: new FormControl('')
+  public title = 'angular-ssn';
+  public ssnSample = new FormGroup({
+    ssn: new FormControl('', [Validators.maxLength(11)])
   })
+
+  public onSubmit(): void {
+    console.log('ssn submitted');
+  }
+
+  public inputSSN(): void {
+    console.log(this.ssnSample.get('ssn')?.valid);
+    this.ssn?.setValue(this.getSSNValidFormat(this.ssn.value))
+  }
+
+  public get ssn(): AbstractControl|null {
+    return this.ssnSample.get('ssn');
+  }
+
+  private getSSNValidFormat(ssn: string): string| null {
+    if (ssn === null || ssn === undefined) { return null; }
+    
+    const pattern = /\d{3}-\d{2}-\d{4}/g;
+    let formattedSSN = ssn as any;
+    const result = pattern.test(formattedSSN);
+    if (!result) {
+      formattedSSN = formattedSSN.match(/\d*/g).join('')
+      .match(/(\d{0,3})(\d{0,2})(\d{0,4})/).slice(1).join('-')
+      .replace(/-*$/g, '');
+    }
+    
+    return formattedSSN;
+  }
+
+  constructor() { }
 }
